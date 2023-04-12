b50c4c95cc1533a35d664aa109706f5cc8e49614
Sergio Rios <serggio.rs@gmail.com>
2023-04-12 08:01:10 -0300

    delete frontend_futApp test

--- frontend_futsApp/src/components/Hero.js
+++
@@ -1,5 +0,0 @@
-import "./Hero.css"
-
-export function Hero() {
-  return <img className="Hero" src="/hero.png" alt="" />;
-}
--- frontend_futsApp/src/components/LoginButton.js
+++
@@ -1,5 +0,0 @@
-import "./LoginButton.css"
-
-export function LoginButton({ onLoginClick }) {
-  return <button className="LoginButton" type="submit" onClick={onLoginClick}>Ingresar</button>
-}
