a086952a1b51655bca20fadb1458246b579dad2f
Sergio Rios <serggio.rs@gmail.com>
2023-04-13 07:39:45 -0300

    username > name, url producction, soccerfield fut7

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-export const API_URL_DEVELOPMENT ='http://localhost:3000/api';
+export const API_URL_DEVELOPMENT ='http://localhost:4000/api';
 
-export const API_URL_PRODUCTION ='https://dev-futsapp.srtesthost.tk/api';
+export const API_URL_PRODUCTION ='http://futapp.com.ar/api';
